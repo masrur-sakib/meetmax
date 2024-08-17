2562acea8af2b888b92eede3faf99b6d2f0de724
masrur-sakib <mashrur333@gmail.com>
2024-08-17 16:42:20 +0600

    friends list generated & profile image added of friends.

---
+++ src/data/friends.js
@@ -0,0 +1,74 @@
+export const users = [
+  {
+    avatar: '/avatars/users/KayleighBysouth.png',
+    name: 'Kayleigh Bysouth',
+  },
+  {
+    avatar: '/avatars/users/Jess Phillips MP.png',
+    name: 'Jess Phillips MP',
+  },
+  {
+    avatar: '/avatars/users/MPEslamHisham.png',
+    name: 'MP Eslam Hisham',
+  },
+  {
+    avatar: '/avatars/users/EduardoFerguson.png',
+    name: 'Eduardo Ferguson',
+  },
+  {
+    avatar: '/avatars/friends/LevizjaVetvendos.png',
+    name: 'Levizja Vetvendos',
+  },
+  {
+    avatar: '/avatars/friends/AfrimKonjufca.png',
+    name: 'Afrim Konjufca',
+  },
+  {
+    avatar: '/avatars/friends/AdayinVetvendosje.png',
+    name: 'Adayin Vetvendosje',
+  },
+  {
+    avatar: '/avatars/friends/AlbiniVjosa.png',
+    name: 'Albini Vjosa',
+  },
+  {
+    avatar: '/avatars/friends/EduardoOrtega.png',
+    name: 'Eduardo Ortega',
+  },
+  {
+    avatar: '/avatars/friends/YasinKawakamu.png',
+    name: 'Yasin Kawakamu',
+  },
+  {
+    avatar: '/avatars/friends/Edilson DeCarvalho.png',
+    name: 'Edilson De Carvalho',
+  },
+  {
+    avatar: '/avatars/friends/AlbiniVjosaGlauki.png',
+    name: 'Albini Vjosa Glauki',
+  },
+  {
+    avatar: '/avatars/friends/MuhametMurteza.png',
+    name: 'Muhamet Murteza',
+  },
+  {
+    avatar: '/avatars/friends/JoseAntonio.png',
+    name: 'Jose Antonio',
+  },
+  {
+    avatar: '/avatars/friends/JessPhillipsMP.png',
+    name: 'Jess Phillips MP',
+  },
+  {
+    avatar: '/avatars/friends/KujtimRushiti.png',
+    name: 'Kujtim Rushiti',
+  },
+  {
+    avatar: '/avatars/friends/OstojaOstojic.png',
+    name: 'Ostoja Ostojic',
+  },
+  {
+    avatar: '/avatars/friends/XhevdetCazimi.png',
+    name: 'Xhevdet Cazimi',
+  },
+];
