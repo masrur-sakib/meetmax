cb0ac2ca56e7d99ad66de15d561ad399d0b7097a
masrur-sakib <mashrur333@gmail.com>
2024-08-18 12:52:54 +0600

    rightSidebar padding modified & scrollbar css not working issue fixed.

--- src/components/Feed/RightSidebar.jsx
+++ src/components/Feed/RightSidebar.jsx
@@ -17,7 +17,7 @@ export default function RightSidebar() {
   });
 
   return (
-    <div className='px-4 rightSidebar sticky top-20 h-[calc(100vh-theme(spacing.20))] overflow-y-auto hidden lg:block w-1/5 text-gray-600'>
+    <div className='px-6 rightSidebar sticky top-20 h-[calc(100vh-theme(spacing.20))] overflow-y-auto hidden lg:block w-1/5 text-gray-600'>
       {/* Search */}
       <div className='flex justify-start items-center space-x-2 w-full h-10 rounded-lg px-4 py-2 ring-1 ring-inset ring-gray-300'>
         <Image
