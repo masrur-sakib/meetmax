da6c51f13c413d47f8a6cf48648dfba1d1150743
masrur-sakib <mashrur333@gmail.com>
2024-08-20 12:21:04 +0600

    bottom menu showing while user is not logged in issue fixed.

--- src/components/BottomMenu.jsx
+++ src/components/BottomMenu.jsx
@@ -3,11 +3,17 @@
 import { useState } from 'react';
 import { bottomMenuData } from '@/data/bottomMenuData';
 import BottomMenuEl from './BottomMenuEl';
+import { useUserStore } from '@/store/store';
 
 export default function BottomMenu() {
+  const user = useUserStore((state) => state.user);
   const [selected, setSelected] = useState('Feed');
   return (
-    <div className='sm:hidden fixed bottom-0 left-0 w-full bg-white border-t border-gray-200 shadow-md flex justify-around z-50'>
+    <div
+      className={`${
+        user ? '' : 'hidden'
+      } sm:hidden fixed bottom-0 left-0 w-full bg-white border-t border-gray-200 shadow-md flex justify-around z-50`}
+    >
       {bottomMenuData.map((data) => (
         <BottomMenuEl key={data.id} data={data} selected={selected} />
       ))}
